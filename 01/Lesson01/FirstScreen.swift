dccc810678bf49c875618c869206420ce3d0c6ef
Anhad Arora <anhadarora@gmail.com>
2015-03-05 16:33:33 -0500

    commit 1
    not a lot done, do not check rn

---
+++ 01/Lesson01/FirstScreen.swift
@@ -0,0 +1,51 @@
+//
+//  FirstScreen.swift
+//  Lesson01
+//
+//  Created by Anhad Arora on 24/02/2015.
+//  Copyright (c) 2015 General Assembly. All rights reserved.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+   
+    
+    
+    
+    
+    //if emailField.isEmpty !=0 &&;  @IBAction func buttonClicked(sender: UIButton)
+    //{
+    //
+    //    let email = emailField.text
+    //
+    //    let password = passwordField.text
+    //
+    //    let errorMessage = checkPassword(email, passwd: password)
+    //
+    //    errorLabel.text = errorMessage
+    //
+    //
+    //
+    //}
+    //func checkPassword(mail: String, passwd: String) -> String
+    //{
+    //    if mail.isEmpty || passwd.isEmpty
+    //    {
+    //        errorLabel.backgroundColor = UIColor.redColor()
+    //        return "Enter Something At Least"
+    //    }
+    //    else
+    //    {
+    //        errorLabel.backgroundColor = UIColor.clearColor()
+    //        return "Proceed"
+    //    }
+    //}
+
+    
+    
+    
+}
+
+
+
