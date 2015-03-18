3482921adc2178395c88a8ce36e73f5b30765b87
Anhad Arora <anhadarora@gmail.com>
2015-03-17 20:41:21 -0400

    homework 4 final commit
    fixed fatal error that passed nil to table cell. all TODOs completed
    successfully

--- 04/Lesson04/MapViewController.swift
+++ 04/Lesson04/MapViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class MapViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
 
 
-    var map = Dictionary<String, String>()
+    var map = [String:String]()
+    //Dictionary<String, String>()
     //var map:NSMutableDictionary = [:]
     
     @IBOutlet weak var textField1: UITextField!
@@ -30,25 +31,23 @@ class MapViewController: UIViewController, UITableViewDataSource, UITableViewDel
         var cell = tableView.dequeueReusableCellWithIdentifier("myMapCell") as UITableViewCell
         
         let person = map.keys.array[indexPath.row]
-        
-        cell.textLabel!.text = person
-        cell.detailTextLabel!.text = map[person]
+        cell.textLabel?.text = person
+        cell.detailTextLabel?.text = map[person]
         
         return cell
     }
 
-
     func textFieldShouldReturn(textField2: UITextField) -> Bool {
+        addData ()
         textField1.resignFirstResponder()
         return true
     }
     
     func addData () {
-        map.keys.array.append(textField1!.text)
-        map.values.array.append(textField2.text)
+        map["\(textField1!.text)"] = textField2?.text
+        
         tableView.reloadData()
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -60,17 +59,6 @@ class MapViewController: UIViewController, UITableViewDataSource, UITableViewDel
         notify.addObserver(self, selector: Selector("kbStuffHappened:"), name: UIKeyboardWillHideNotification, object: nil)
     }
     
-    
-//    func keyboardWillShow(notification : NSNotification){
-//        textField1.backgroundColor = UIColor.blueColor()
-//        textField2.backgroundColor = UIColor.blueColor()
-//    }
-//    
-//    func keyboardWillHide(notification : NSNotification){
-//        textField1.backgroundColor = UIColor.redColor()
-//        textField2.backgroundColor = UIColor.redColor()
-//    }
-    
     func kbStuffHappened(notification : NSNotification){
         if notification.name == UIKeyboardWillHideNotification {
             textField1.backgroundColor = UIColor.redColor()
@@ -79,11 +67,6 @@ class MapViewController: UIViewController, UITableViewDataSource, UITableViewDel
             textField1.backgroundColor = UIColor.blueColor()
             textField2.backgroundColor = UIColor.blueColor()
         }
-        /*
-        TODO three: Add TWO text views and a table view to this view controller, either using code or storybaord. Accept keyboard input from the two text views. When the 'return' button is pressed on the SECOND text view, add the text view data to a dictionary. The KEY in the dictionary should be the string in the first text view, the VALUE should be the second.
-        TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
-        TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
-        */
     }
 }
 
