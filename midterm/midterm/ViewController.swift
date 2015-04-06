2bbf8074c730ebf043e9dd85076ddbf964ccf420
Anhad Arora <anhad@172-16-215-219.DYNAPOOL.NYU.EDU>
2015-04-06 15:20:35 -0400

    final commit
    ready for assessment. all bugs fixed

--- midterm/midterm/ViewController.swift
+++ midterm/midterm/ViewController.swift
@@ -18,9 +18,13 @@ class ViewController: UIViewController {
         numberFunction(theButton.titleLabel!.text!)
     }
     
-    var storeString = ""
+    var storeString = "" //string variable to store intermediate value being used in the calculations
+    var firstOperand = Double()
+    var operatorSign = ""
+    var secondOperand = Double()
+    
+    //function that inputs numbers, ie, 0...9 and decimal point "."
     func numberFunction (buttonText: String){
-        
         switch buttonText {
         case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".":
             storeString = storeString + buttonText
@@ -30,25 +34,20 @@ class ViewController: UIViewController {
         ioLabel.text = storeString
     }
     
+    //function to implement the negation key, "+/-"
     @IBAction func plusMinusTapped(sender: AnyObject) {
         firstOperand = 0.0
         operatorSign = "−"
         secondOperand = (ioLabel.text! as NSString).doubleValue
         firstOperand = calculateAnswer(firstOperand, second: secondOperand, opSymbol: operatorSign)
-        
-        
-        
         storeString = ("\(firstOperand)")
         ioLabel.text = storeString
         operatorSign = ""
         secondOperand = 0.0
-
+        
     }
     
-    
-    var firstOperand = Double()
-    var operatorSign = ""
-    var secondOperand = Double()
+    //function that inputs operator keys and simultneously computes the answer as the key is pressed
     @IBAction func operatorButtonTapped(operatorButton: UIButton) {
         if operatorSign == "" {
             firstOperand = (storeString as NSString).doubleValue
@@ -71,6 +70,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //universal function that calculates answer whereever required
     func calculateAnswer (first: Double, second: Double, opSymbol: String) -> Double {
         var answer = Double()
         switch opSymbol {
@@ -88,6 +88,7 @@ class ViewController: UIViewController {
         return answer
     }
     
+    //function that computes the final answer as a result of successive input numbers and operators
     @IBAction func equalsButtonTapped(sender: AnyObject) {
         secondOperand = (storeString as NSString).doubleValue
         var finalAnswer = calculateAnswer(firstOperand, second: secondOperand, opSymbol: operatorSign)
@@ -99,6 +100,9 @@ class ViewController: UIViewController {
         }
     }
     
+    //function that calculates the second number as a percentage of the first number
+    //this function first shows the user a percentage of the first number,
+    //then performs arithmetic operation on the first number and outputs the result on pressing the "=" button
     @IBAction func percentageButton(sender: AnyObject) {
         secondOperand = (storeString as NSString).doubleValue
         secondOperand = (secondOperand/100) * firstOperand
@@ -106,6 +110,7 @@ class ViewController: UIViewController {
         storeString = "\(secondOperand)"
     }
     
+    //this function sets all variables to an initial value so new computation can take place
     @IBAction func allClear(sender: AnyObject) {
         ioLabel.text = "0"
         storeString = ""
